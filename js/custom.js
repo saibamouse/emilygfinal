3f6ccee541bcb3a4b3a351b4944a5dca78ef2aa1
saibamouse <s3913217@student.rmit.edu.au>
2021-06-20 13:12:39 +1000

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -10,7 +10,9 @@ $(window).load(function(){
 $(function(){
     jQuery(document).ready(function() {
 		$('body').backstretch([
-	 
+      "images/tm-bg-slide-1.jpg",
+      "images/tm-bg-slide-2.jpg",
+      "images/tm-bg-slide-3.jpg"
 	 			], 	{duration: 3200, fade: 1300});
 		});
 })
